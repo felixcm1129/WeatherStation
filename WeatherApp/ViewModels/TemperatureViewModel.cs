053ee5eff86a3fae9462c904a06eecf4b63fa61b
Felix CarleMilette <1539902@cshawi.ca>
2020-10-29 17:09:16 -0400

    T01 CelsisInFahrenheit_AlwaysReturnGoodValue : Done

--- WeatherApp/ViewModels/TemperatureViewModel.cs
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WeatherApp.Commands;
 
 namespace WeatherApp.ViewModels
 {
     public class TemperatureViewModel : BaseViewModel
     {
         /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet
+       
     }
 }
